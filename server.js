4cfc9ad163491f9dc3ea9b45b50131e93dac1cd1
Perluceo <FullStackDevSMU@gmail.com>
2019-08-07 23:04:50 -0500

    syntax errors on server.js resolved and now connects local:host

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var PORT = process.env.PORT || 3000;
 var app = express();
 var router = express.Router();
-app.use(express.static(_dirname + "/public");
+app.use(express.static(__dirname + "/public"));
 app.use(router);
 
 app.listen(PORT, function() {
