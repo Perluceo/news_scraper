0c6ca4333716ca18597f06fc0f7720ca991a4d78
Perluceo <FullStackDevSMU@gmail.com>
2019-08-08 01:12:13 -0500

    home/saved/server.js route

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3000;
 var app = express();
 var router = express.Router();
+require("./config/route")(router);
 app.use(express.static(__dirname + "/public"));
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
